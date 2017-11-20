7c31c216045ebdb756407a5f2b9a653da0069467
moisesHerrera01 <moises.oct@gmail.com>
2017-11-20 14:35:41 -0600

    comenta cascada

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,5 @@
 class Activity < ApplicationRecord
     belongs_to  :zone
     has_many :collections
+    has_many :resource_activity
 end
--- app/models/tree.rb
+++ app/models/tree.rb
@@ -1,5 +1,5 @@
 class Tree < ApplicationRecord
 	has_many :detail_income 
-	has_many :resource_activities_tree dependent: :destroy
+	#has_many :resource_activities_tree, dependent: :destroy
 
 end
