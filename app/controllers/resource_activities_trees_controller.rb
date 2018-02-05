b4a8887e1f4cab6394acc1bd505b3d20c551bcc9
moisesHerrera01 <moises.oct@gmail.com>
2018-02-04 22:00:55 -0600

    resource_activities_tree coherencia de datos para navegabilidad

--- app/controllers/resource_activities_trees_controller.rb
+++ app/controllers/resource_activities_trees_controller.rb
@@ -2,12 +2,13 @@ class ResourceActivitiesTreesController < ApplicationController
 
 	#GET /resource activities tree
     def index
-        @resource_activities_trees = ResourceActivitiesTree.all.order('id DESC')
+        @resource_activities_trees = ResourceActivitiesTree.where(resource_activities_trees: {resource_activity_id: params[:resource_activity_id]})
     end
 
     #GET /resource activities tree/new
     def new
         @resource_activity_id = params[:resource_activity_id]
+        @activity_id = params[:activity_id]
         @resource_activities_tree = ResourceActivitiesTree.new
     end
 
@@ -15,7 +16,8 @@ class ResourceActivitiesTreesController < ApplicationController
     def create
         @resource_activities_tree = ResourceActivitiesTree.new(resource_activities_tree_params);
         if @resource_activities_tree.save
-            redirect_to :action => 'index'
+            redirect_to :action => 'index', :resource_activity_id=>params[:resource_activities_tree][:resource_activity_id],
+            :activity_id=>params[:resource_activities_tree][:activity_id]
         else
             render :new
         end
@@ -23,6 +25,7 @@ class ResourceActivitiesTreesController < ApplicationController
 
     #GET /resource_activity/:id/edit
     def edit
+        @activity_id = params[:activity_id]
         @resource_activities_tree = ResourceActivitiesTree.find(params[:id])
     end
 
@@ -30,14 +33,15 @@ class ResourceActivitiesTreesController < ApplicationController
     def update
         resource_activities_tree = ResourceActivitiesTree.find(params[:resource_activities_tree][:id])
         resource_activities_tree.update_attributes(resource_activities_tree_params)
-        redirect_to :action => 'index'
+        redirect_to :action => 'index', :resource_activity_id=>params[:resource_activities_tree][:resource_activity_id],
+        :activity_id=>params[:resource_activities_tree][:activity_id]
     end
 
     # DELETE /resource_activity
     def destroy
         resource_activities_tree = ResourceActivitiesTree.find(params[:id])
         resource_activities_tree.destroy
-        redirect_to :action => 'index'
+        redirect_to :action => 'index', :resource_activity_id=>params[:resource_activity_id], :activity_id=>params[:activity_id]
     end
 
     private
